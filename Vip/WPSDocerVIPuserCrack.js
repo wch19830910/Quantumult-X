cb6680c76530c24eeff63307974aef914c043c05
wch19830910 <75171072+wch19830910@users.noreply.github.com>
2022-07-09 12:23:18 +0800

    Update WPSDocerVIPuserCrack.js

--- Vip/WPSDocerVIPuserCrack.js
+++ Vip/WPSDocerVIPuserCrack.js
@@ -3,15 +3,15 @@
 下载地址：http://t.cn/A6ZD2hl3
 脚本作者：Hausd0rff
 更新时间：2022-07-05
-脚本发布：https://t.me/yqc_123
+脚本发布：https://raw.githubusercontent.com/wch19830910/Quantumult-X/main/Vip/WPSDocerVIPuserCrack.js
 问题反馈：https://t.me/yqc_777
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 [rewrite_local]
 # > WPS解锁稻壳会员
-^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPuserCrack.js
-^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPowerCrack.js
+^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/wch19830910/Quantumult-X/main/Vip/WPSDocerVIPuserCrack.js
+^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/wch19830910/Quantumult-X/main/Vip/WPSDocerVIPuserCrack.js
 
 [mitm] 
 hostname = *.docer.wps.cn, vipapi.wps.cn, account.wps.cn
