9d1ddbb71afcc6e2b6503e26fa1a7113bd8b3ebc
wch19830910 <75171072+wch19830910@users.noreply.github.com>
2022-11-17 16:17:35 +0800

    Update APTV.js

--- Vip/APTV.js
+++ Vip/APTV.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/APTV.js
+https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/wch19830910/Quantumult-X/main/Vip/APTV.js
 
 [mitm] 
 
