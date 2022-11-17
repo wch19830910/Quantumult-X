2daa257bb2321a168eaf7f6bd64e92396a894609
wch19830910 <75171072+wch19830910@users.noreply.github.com>
2022-11-17 16:23:35 +0800

    Update DiDaQingDan.js

--- Vip/DiDaQingDan.js
+++ Vip/DiDaQingDan.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/dida365\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ddqd.js
+^https:\/\/dida365\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/wch19830910/Quantumult-X/main/Vip/DiDaQingDan.js
 
 [mitm] 
 
