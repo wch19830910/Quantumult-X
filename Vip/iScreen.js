ae1bf38c314b4da4a690ce93e1351f929435f87f
wch19830910 <75171072+wch19830910@users.noreply.github.com>
2022-11-17 16:19:19 +0800

    Update iScreen.js

--- Vip/iScreen.js
+++ Vip/iScreen.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/cs\.kuso\.xyz\/ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/iScreen.js
+^https:\/\/cs\.kuso\.xyz\/ url script-response-body https://raw.githubusercontent.com/wch19830910/Quantumult-X/main/Vip/iScreen.js
 
 [mitm] 
 
