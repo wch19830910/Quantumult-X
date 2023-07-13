0ad0228d402972973b749780123382544b9869d6
wch19830910 <75171072+wch19830910@users.noreply.github.com>
2023-07-13 13:27:14 +0800

    Update HMT2.js

--- Task/HMT/HMT2.js
+++ Task/HMT/HMT2.js
@@ -1,3 +1,15 @@
+/*
+
+cookie获取:打开app，我的，点击等级获取成功，即可注释点cookie获取脚本
+[rewrite_local]
+#获取鹤鸣亭Cookie
+^https:\/\/qfapi\.hmting\.com\/store\/grade\/grade-list url script-request-header https://raw.githubusercontent.com/wch19830910/Quantumult-X/main/Task/HMT/HMTCK.js
+
+[task_local]
+11 0 * * * https://raw.githubusercontent.com/wch19830910/Quantumult-X/main/Task/HMT/HMT1.js, tag=鹤鸣亭签到, img-url=https://raw.githubusercontent.com/wch19830910/wch/main/PNG/HMT.JPG, enabled=true
+12 0 * * * https://raw.githubusercontent.com/wch19830910/Quantumult-X/main/Task/HMT/HMT2.js, tag=鹤鸣亭签到日志, img-url=https://raw.githubusercontent.com/wch19830910/wch/main/PNG/HMT.JPG, enabled=true
+*/
+
 const cookieName = '鹤鸣亭'
 const signurlKey = 'photonmang_signurl_HMT'
 const signheaderKey = 'photonmang_signheader_HMT'
