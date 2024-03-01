f75e2ed204824be1f88d94db31aa81b084d74bc9
wch19830910 <75171072+wch19830910@users.noreply.github.com>
2024-03-01 16:39:22 +0800

    Update STSQ.js

--- Vip/soutushenqi/STSQ.js
+++ Vip/soutushenqi/STSQ.js
@@ -14,7 +14,7 @@
 *******************************
 [rewrite_local]
 # > 搜图神器解锁会员
-^https?:\/\/wallpaper\.soutushenqi\.com\/api\/v1\/account\/token$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/SearchPictureCrack.js
+^https?:\/\/wallpaper\.soutushenqi\.com\/api\/v1\/account\/token$ url script-response-body https://raw.githubusercontent.com/wch19830910/Quantumult-X/main/Vip/soutushenqi/STSQ.js
 
 [mitm] 
 hostname = wallpaper.soutushenqi.com
